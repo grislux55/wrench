2f041db4cce63ea817d0b29aa60c4bbeb44c953d
Roc <roc.gui@foxmail.com>
2022-12-06 18:34:44 +0800

    [implement]utils/encode: Implement encode logic
    Signed-off-by: Roc <roc.gui@foxmail.com>

--- includes/utils/encode.h
+++ includes/utils/encode.h
@@ -4,7 +4,28 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// Note: out MUST BE NULL
+/**
+ * @brief               将输入信息序列进行编码
+ * @param out           输出的字节序列
+ * @param in            输入的需要编码数据
+ * @param in_len        输入数据长度
+ * @return              编码后的数据长度
+ * @note                函数会根据数据长度自动分配*out的空间
+ *                      不会尝试覆盖输出变量的内容，如果*out不为NULL则什么都不会干
+ * @par 示例
+ * @code
+ *     uint8_t source[] = {0xca, 0xfe, 0xba, 0xbe};
+ *     uint8_t target[] = {0x00, 0xcb, 0x7f, 0xaf, 0x57, 0xef, 0x02};
+ *
+ *     uint8_t *encoded = NULL;
+ *     size_t encoded_len = encode(&encoded, source, 4);
+ *     assert(encoded_len == 7);
+ *     for (int i = 0; i < encoded_len; i++) {
+ *         assert(encoded[i] == target[i]);
+ *     }
+ * @endcode
+ */
+
 extern size_t encode(uint8_t **out, const uint8_t *in, size_t in_len);
 
 #endif //WRENCH_ENCODE_H
--- src/utils/encode.c
+++ src/utils/encode.c
@@ -1,5 +1,56 @@
 #include "utils/encode.h"
 
-size_t encode(uint8_t **out, const uint8_t *in, size_t in_len) {
-    return 0;
+#include <malloc.h>
+#include <string.h>
+
+size_t encode(uint8_t **out, const uint8_t *in, size_t in_len)
+{
+    // 传入不需要的数据的时候直接返回防止覆盖
+    if (out == NULL || *out != NULL) {
+        return 0;
+    }
+
+    // 根据内部数据的长度计算出需要多长的空间来存储编码后的数据
+    const size_t target_len = (8 * in_len + 7 - 1) / 7 + 2;
+
+    // 没有需要编码的数据
+    if (target_len <= 0) {
+        return 0;
+    }
+
+    *out = malloc(target_len);
+    memset(*out, 0, target_len);
+    uint8_t *out_converted = *out;
+
+    out_converted[0] = 0x00;
+
+    size_t wrote_bit = 8;
+    for (size_t i = 0; i < in_len; i++) {
+        // 对于1100 1010 1111 1110
+        // 每从高位开始写入，每过七个位补充一位flag位（数据flag为1）
+        // 分组 1100 101_ 0111 11__
+        // 补充flag 1100 1011 0111 11x1
+        for (int j = 7; j >= 0; j--) {
+            if (wrote_bit % 8 == 7) {
+                // wrote_bit % 8 代表写入哪一位
+                // 每个字节的第8位值置1
+                out_converted[wrote_bit / 8] |= 1 << (7 - (wrote_bit % 8));
+                wrote_bit++;
+            }
+            if (in[i] & (1 << j)) {
+                out_converted[wrote_bit / 8] |= 1 << (7 - (wrote_bit % 8));
+            }
+            wrote_bit++;
+        }
+    }
+
+    // 空位用1填充
+    while (wrote_bit / 8 != target_len - 1) {
+        out_converted[wrote_bit / 8] |= 1 << (7 - (wrote_bit % 8));
+        wrote_bit++;
+    }
+
+    out_converted[target_len - 1] = 0x02;
+
+    return target_len;
 }
