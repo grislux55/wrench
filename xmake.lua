228dc96679111d13189f42c2c02274fde5d95887
grislux55 <jebchou.hg@gmail.com>
2022-12-27 15:22:20 +0800

    [config]xmake: Fix MSVC compiling warning
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,14 @@
 set_project("wrench")
 set_xmakever("2.7.3")
 
-set_warnings("all", "error")
+set_warnings("allextra", "error")
 set_languages("c17", "c++20")
 
 add_rules("mode.release", "mode.debug")
 
-add_cxxflags("-Wall","-Wextra","-Wno-sign-compare","-Wno-unused-function","-Wno-unused-const-variable")
+add_cxxflags("-Wno-sign-compare", {tools = {"gcc", "clang"}})
+add_cxxflags("-Wno-unused-function", {tools = {"gcc", "clang"}})
+add_cxxflags("-Wno-unused-const-variable", {tools = {"gcc", "clang"}})
 
 add_requires("hiredis 1.0.2", "toml++ 3.2.0", "gtest 1.12.1", "argparse 2.6.0")
 
