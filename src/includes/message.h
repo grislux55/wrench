d454ab121b791609b0ceb490ac0e49e89cd3e199
grislux55 <jebchou.hg@gmail.com>
2023-03-01 15:12:21 +0800

    [chore]message: Standardize code
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- src/includes/message.h
+++ src/includes/message.h
@@ -1,115 +1,113 @@
-#ifndef WRENCH_DEAMON_MESSGE_H
+﻿#ifndef WRENCH_DEAMON_MESSGE_H
 #define WRENCH_DEAMON_MESSGE_H
 
 #include <string>
 
-using namespace std;
-
 // 拧紧系统发送请求连接扳手
 typedef struct {
-    string stationIp;
-    string taskId;
-    string wrenchName;
-    string wrenchSerial;
-} Connect_Resquest_Msg;
+    std::string stationIp;
+    std::string taskId;
+    std::string wrenchName;
+    std::string wrenchSerial;
+} ConnectResquestMsg;
 
 typedef struct {
-    string msgId;
-    string msgType;
-    string handlerName;
-    string currentTime;
-    Connect_Resquest_Msg msgTxt;
-} Connect_Resqust;
+    std::string msgId;
+    std::string msgType;
+    std::string handlerName;
+    std::string currentTime;
+    ConnectResquestMsg msgTxt;
+} ConnectResqust;
 
 // 扳手回写给拧紧系统
 typedef struct {
-    string wrenchName;
-    string wrenchSerial;
-    string status;
-    string desc;
-    string currentTime;
-    string taskId;
-} Connect_Response_Msg;
+    std::string wrenchName;
+    std::string wrenchSerial;
+    std::string status;
+    std::string desc;
+    std::string currentTime;
+    std::string taskId;
+} ConnectResponseMsg;
 
 typedef struct {
-    string msgType;
-    string msgId;
-    string handlerName;
-    Connect_Response_Msg msgTxt;
-} Connect_Response;
+    std::string msgType;
+    std::string msgId;
+    std::string handlerName;
+    ConnectResponseMsg msgTxt;
+} ConnectResponse;
 
 // 拧紧系统发送任务数据给扳手
 typedef struct {
-    string stationIp;
-    string taskId;
-    string taskDetailId;
-    string taskDesc;
-    string wrenchSerial;
-    string wrenchSerialDesc;
-    string userId;
-    string userDesc;
-    string controlMode;
-    string workMode;
-    string boltNum;
-    string repeatCount;
-    string target;
-    string monitor;
-    string torque;
-    string torqueDeviationUp;
-    string torqueDeviationDown;
-    string torqueAngleStart;
-    string angle;
-    string angleDeviationUp;
-    string angleDeviationDown;
-    string unit;
-} Task_Request_Msg;
+    std::string stationIp;
+    std::string taskId;
+    std::string taskDetailId;
+    std::string taskDesc;
+    std::string wrenchSerial;
+    std::string wrenchSerialDesc;
+    std::string userId;
+    std::string userDesc;
+    std::string controlMode;
+    std::string workMode;
+    std::string boltNum;
+    std::string repeatCount;
+    std::string target;
+    std::string monitor;
+    std::string torque;
+    std::string torqueDeviationUp;
+    std::string torqueDeviationDown;
+    std::string torqueAngleStart;
+    std::string angle;
+    std::string angleDeviationUp;
+    std::string angleDeviationDown;
+    std::string unit;
+} TaskRequestMsg;
 
 typedef struct {
-    string msgId;
-    string msgType;
-    string handlerName;
-    string currentTime;
-    Task_Request_Msg msgTxt;
-} Task_Request;
+    std::string msgId;
+    std::string msgType;
+    std::string handlerName;
+    std::string currentTime;
+    TaskRequestMsg msgTxt;
+} TaskRequest;
 
 // 拧紧系统收到扳手的任务数据回写
 typedef struct {
-    string wrenchSerial;
-    string currentTime;
-    string status;
-    string desc;
-} Task_Response_Msg;
+    std::string wrenchSerial;
+    std::string currentTime;
+    std::string status;
+    std::string desc;
+} TaskResponseMsg;
 
 typedef struct {
-    string msgId;
-    string msgType;
-    string handlerName;
-    Task_Response_Msg msgTxt;
-} Task_Response;
+    std::string msgId;
+    std::string msgType;
+    std::string handlerName;
+    TaskResponseMsg msgTxt;
+} TaskResponse;
 
 // 拧紧系统采集扳手作业数据
 typedef struct {
-    string msgId;
-    string taskId;
-    string taskDetailId;
-    string wrenchSerial;
-    string torque;
-    string angle;
-    string status;
-    string consumeTime;
-    string desc;
-    string startDate;
-    string endDate;
-    string workTime;
-    string currentTime;
-} Task_Status_Msg;
+    std::string msgId;
+    std::string taskId;
+    std::string taskDetailId;
+    std::string wrenchSerial;
+    std::string torque;
+    std::string angle;
+    std::string status;
+    std::string consumeTime;
+    std::string desc;
+    std::string startDate;
+    std::string endDate;
+    std::string workTime;
+    std::string currentTime;
+} TaskStatusMsg;
 
 typedef struct {
-    string msgId;
-    string msgType;
-    string handlerName;
-    Task_Status_Msg msgTxt;
-} Task_Status;
+    std::string msgId;
+    std::string msgType;
+    std::string handlerName;
+    TaskStatusMsg msgTxt;
+} TaskStatus;
 
 // TODO 拧紧系统采集扳手基本信息/充电状态/异常/断开连接数据
 // TODO 拧紧系统采集扳手数据消息后发送给扳手
